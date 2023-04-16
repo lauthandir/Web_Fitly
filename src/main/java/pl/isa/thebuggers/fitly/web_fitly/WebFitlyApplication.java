9ce7fc53a62d8630b470500fb6dce6ccb615cbdf
Wojciech Walczak <wwalczakpraca@wp.pl>
2023-04-16 20:38:03 +0200

    Initial commit

---
+++ src/main/java/pl/isa/thebuggers/fitly/web_fitly/WebFitlyApplication.java
@@ -0,0 +1,13 @@
+package pl.isa.thebuggers.fitly.web_fitly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFitlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFitlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/isa/thebuggers/fitly/web_fitly/WebFitlyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.isa.thebuggers.fitly.web_fitly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFitlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
